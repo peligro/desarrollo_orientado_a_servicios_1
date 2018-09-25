4f2214542ca24f55264e1d7ea59a1af3210a5e81
César Cancino <yo@cesarcancino.com>
2018-09-25 16:30:16 -0300

    módulo categorías, corrección controlador Acceso :facepunch:

--- backend/app/Http/Controllers/AccesoController.php
+++ backend/app/Http/Controllers/AccesoController.php
@@ -42,50 +42,7 @@ class AccesoController extends Controller
         return redirect('/acceso/login');
         }
     }
-    public function logueado()
-    {
-        if (Auth::check()) {
-            return view('acceso.logueado');
-        }else
-        {
-        	//return "no no";
-        	return redirect('/acceso/login');
-        }
-
-        
-    }
-    public function registro()
-    {
-        return view('acceso.registro');
-    }
-    public function registro_post(Request $request)
-    {
-    	$this->validate
-        (
-        	$request,
-        	[
-        		'nombre'=>'required|max:255',
-        		'correo'=>'required|email|unique:users,email',
-        		'pass'=>'required|min:6|confirmed'
-        	],
-        	[
-        		'nombre.required'=>'El campo Nombre está vacío',
-        		'correo.required'=>'El campo E-Mail está vacío',
-        		'correo.email'=>'El E-Mail ingresado no tiene un formato válido',
-        		'correo.unique'=>'El E-Mail ingresado ya está siendo usado por otro usuario',
-        		'pass.required'=>'El campo Contraseña está vacío',
-        		'pass.confirmed'=>'Las contraseñas ingresadas no coiciden',
-        	]
-        );
-        User::create([
-            'name' => $request->input('nombre'),
-            'email' => $request->input('correo'),
-            'password' => bcrypt($request->input('pass')),
-        ]);
-        $request->session()->flash('css', 'success');
-        $request->session()->flash('mensaje', 'Se ha agregado el registro exitosamente');
-        return redirect('/acceso/registro');
-    }
+    
     public function salir(Request $request)
     {
         Auth::logout();
